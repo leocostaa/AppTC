0e2317057ab52c5ff618464c875171ed2ef41fa8
leocostaa <32634126+leocostaa@users.noreply.github.com>
2020-05-09 23:00:50 +0100

    Criação de uma pagina nova já +- concluida , inicialização dos buttons e interligação dos mesmos

--- app/src/main/java/com/example/apptc/MainActivity.java
+++ app/src/main/java/com/example/apptc/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.apptc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+    public void StartConsultarPaciente ( View view){
+        Intent MainActivity = new Intent(this,MainActivityConsultarPaciente.class);
+        startActivity(MainActivity);
+
+    }
+    public void StartConsularMedicacao (View view){
+        Intent MainActivity = new Intent(this,MainActivityConsultarMedicacao.class);
+        startActivity(MainActivity);
+
+    }
+
+    public void StartAddPaciente ( View view){
+        Intent MainActivity = new Intent(this,MainActivityAddPaciente.class);
+        startActivity(MainActivity);
+
+    }
+    public void StartAddMedicacao ( View view){
+        Intent MainActivity = new Intent(this,MainActivityAddMedicacao.class);
+        startActivity(MainActivity);
+
+    }
+
 }
